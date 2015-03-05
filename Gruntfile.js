e1a6c3d01c9aa9418bd7bbbb57909c022426196e
James Feigel <james.feigel@sq1.com>
2015-03-04 23:12:05 -0600

    Initial Angular commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,30 +3,38 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		clean: ["covervid.min.js"],
+        connect: {
+			server: {
+				base: '/',
+				hostname: 'localhost'
+			}
+		},
+
+		clean: ["ngCovervid.min.js"],
 
 		uglify: {
 			dist: {
 				files: {
-					'covervid.min.js': ['covervid.js']
+					'ngCovervid.min.js': ['ngCovervid.js']
 				}
 			}
 		},
 
 		watch: {
 			dist: {
-				files: 'covervid.js',
+				files: 'ngCovervid.js',
 				tasks: ['clean', 'uglify', 'watch']
 			}
 		}
 
 	});
 
+    grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['clean', 'uglify', 'watch']);
+	grunt.registerTask('default', ['clean', 'uglify', 'connect:server', 'watch']);
 	grunt.registerTask('publish', ['clean', 'uglify']);
 
 };
